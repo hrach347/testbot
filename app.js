e210e5918a3af8a96745bb1a51479ef3830dd08f
hrach347 <hrachpapyan347@gmail.com>
2024-09-14 16:40:35 +0400

    test

---
+++ app.js
@@ -0,0 +1,23 @@
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const client = new Client();
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message_create", (msg) => {
+  console.log("message detected");
+  if (msg.body === "reply me") {
+    msg.reply("REPLIED TEXT TEST");
+  } else if (msg.body === "send") {
+    client.sendMessage("37498222474@c.us", "test arame");
+  }
+});
+
+client.initialize();
